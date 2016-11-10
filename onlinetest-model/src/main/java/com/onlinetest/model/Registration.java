05e36c28b5be3182f06a002d2d4999bcb04c3e99
virendra <vspms1983@gmail.com>
2016-11-10 12:55:30 +0530

    changes in Regisstration from

--- onlinetest-model/src/main/java/com/onlinetest/model/Registration.java
+++ onlinetest-model/src/main/java/com/onlinetest/model/Registration.java
@@ -1,8 +1,11 @@
 package com.onlinetest.model;
 
 public class Registration {
+	
 	private String firatName;
 	private String lastName;
+	private String date;
+	private String contactPersonName;
 	
 	public String getFiratName() {
 		return firatName;
@@ -16,6 +19,19 @@ public class Registration {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getContactPersonName() {
+		return contactPersonName;
+	}
+	public void setContactPersonName(String contactPersonName) {
+		this.contactPersonName = contactPersonName;
+	}
+	
 	
 
 }
