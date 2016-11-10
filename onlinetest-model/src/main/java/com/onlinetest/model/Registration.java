42979ac4766c6637b3411c2eae054237ae3dcf88
virendra <vspms1983@gmail.com>
2016-11-10 12:34:23 +0530

    my first branch commit

---
+++ onlinetest-model/src/main/java/com/onlinetest/model/Registration.java
@@ -0,0 +1,21 @@
+package com.onlinetest.model;
+
+public class Registration {
+	private String firatName;
+	private String lastName;
+	
+	public String getFiratName() {
+		return firatName;
+	}
+	public void setFiratName(String firatName) {
+		this.firatName = firatName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+
+}
